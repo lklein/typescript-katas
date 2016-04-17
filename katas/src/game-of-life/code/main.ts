4b17d657cb83448bcdcf2b8353d00900fb54a313
VictorArcas <varcas@customerville.com>
2016-04-17 11:42:20 +0200

    Add rules to take in consideration. Add next state mutation logic

--- katas/src/game-of-life/code/main.ts
+++ katas/src/game-of-life/code/main.ts
@@ -1,11 +1,43 @@
 /// <reference path="./cell.ts" />
 
 class GameOfLife{
+  private static minimumToSurvive: number = 2;
   private static perfectCase: number = 3;
+  
+  private environment: Array<Array<Cell>>;
+  private futureEnvironment: Array<Array<Cell>>;
+  
+  constructor(environment: Array<Array<Cell>>){
+    this.environment = environment;
+    this.futureEnvironment = [];
+  }
+  
+  next = (): Array<Array<Cell>> => {
+    this.environment.forEach((row: Array<Cell>, rowIndex: number) => {
+      let futureRow = row.reduce(
+          (futureRow:Array<Cell>, cell: Cell, cellIndex: number) => {
+            futureRow.push(this.getFutureCell([rowIndex, cellIndex]));
+            return futureRow;
+        }, []);
+      this.futureEnvironment.push(futureRow);
+    });
+    
+    this.environment = this.futureEnvironment;
+    this.futureEnvironment = [];
+    return this.environment;
+  }
+  
+  getFutureCell = (position: Array<number>): Cell => {
+    let [y, x] = position,
+        willBeAlive: boolean = GameOfLife.evaluate(this.environment, position);
+    
+    return new Cell(willBeAlive);
+  }
   
   static evaluate(environment: Array<Array<Cell>>, position: Array<number>){
     let [y, x] = position;
-    let neighbours = 0,
+    let cell: Cell = environment[y][x],
+        neighbours: number = 0,
         environmentWidth: number = environment[y].length,
         top: number = y - 1,
         left: number = x - 1,
@@ -19,7 +51,7 @@ class GameOfLife{
     if(hasTop){
       let topPortion: Array<Cell> = environment[top].slice(
           hasLeft ? left : x, 
-          hasRight ? (right + 1) : x );
+          hasRight ? (right + 1) : x + 1 );
       neighbours += this.countNeighboursInRow(topPortion);
     }
     if(hasLeft && environment[y][left].alive){
@@ -31,11 +63,11 @@ class GameOfLife{
     if(hasBottom){
       let bottomPortion: Array<Cell> = environment[bottom].slice(
           hasLeft ? left : x, 
-          hasRight ? (right + 1) : x );
+          hasRight ? (right + 1) : x + 1 );
       neighbours += this.countNeighboursInRow(bottomPortion);
     }
     
-    return this.willBeAlive(neighbours);
+    return this.willBeAlive(cell, neighbours);
   }
   
   static countNeighboursInRow = (row: Array<Cell>): number => {
@@ -44,7 +76,15 @@ class GameOfLife{
       }, 0);
   }
   
-  static willBeAlive = (neighbours: number): boolean => {
-    return neighbours === GameOfLife.perfectCase;
+  static willBeAlive = (cell: Cell, neighbours: number): boolean => {
+    if(neighbours === GameOfLife.perfectCase){
+      return true;
+    }
+    
+    if(cell.alive){
+      return neighbours === GameOfLife.minimumToSurvive;
+    }
+    
+    return false;
   }
 }
--- katas/src/game-of-life/spec/mainSpec.ts
+++ katas/src/game-of-life/spec/mainSpec.ts
@@ -5,36 +5,110 @@
 describe("Game of Life kata", () => {
   describe("Empty cell behaviour", () => {
     let fertile = new Cell(false),
-        overpopulated = new Cell(false),
         infertile = new Cell(false),
         
-        matrix: Array<Array<Cell>> = [
+        environment: Array<Array<Cell>> = [
           [new Cell(true), new Cell(false), new Cell(true), infertile],
-          [fertile,        new Cell(true),  overpopulated,  new Cell(true)],
+          [fertile,        new Cell(true),  new Cell(false),  new Cell(true)],
           [new Cell(true), new Cell(false), new Cell(true), new Cell(false)],
         ];
     
     it("Must become alive if has 3 neighbours", () => {
-      let result: boolean = GameOfLife.evaluate(matrix, [1, 0]),
+      let result: boolean = GameOfLife.evaluate(environment, [1, 0]),
           expected: boolean = true;
       
       expect(result).toBe(expected);
     });
     
     it("Must remain empty if has less than 3 neighbours", () => {
-      let result: boolean = GameOfLife.evaluate(matrix, [0, 3]),
+      let result: boolean = GameOfLife.evaluate(environment, [0, 3]),
           expected: boolean = false;
       
       expect(result).toBe(expected);
     });
     
     it("Must remain empty if has more that 3 neighbours", () => {
-      let result: boolean = GameOfLife.evaluate(matrix, [1, 2]),
+      let result: boolean = GameOfLife.evaluate(environment, [1, 2]),
           expected: boolean = false;
       
       expect(result).toBe(expected);
     });
   });
+  
+  describe("Inhabited cell behaviour", () => {
+    let maxRegular = new Cell(true),
+        minRegular = new Cell(true),
+        overPopulated = new Cell(true),
+        
+        environment: Array<Array<Cell>> = [
+          [new Cell(true), new Cell(false), new Cell(true),   minRegular],
+          [maxRegular,     new Cell(true),  overPopulated,  new Cell(true)],
+          [new Cell(true), new Cell(false), new Cell(true),   new Cell(false)],
+        ];
+    
+    it("Must remain alive if has 2 neighbours", () => {
+      let expected: boolean = true,
+          result: boolean = GameOfLife.evaluate(environment, [0, 3]);
+          
+      expect(result).toBe(expected);
+    });
+    
+    it("Must remain alive if has 3 neighbours", () => {
+      let expected: boolean = true,
+          result: boolean = GameOfLife.evaluate(environment, [1, 0]);
+          
+      expect(result).toBe(expected);
+    });
+    
+    it("Must become empty if has more than 3 neighbours", () => {
+      let expected: boolean = false,
+          result: boolean = GameOfLife.evaluate(environment, [1, 2]);
+          
+      expect(result).toBe(expected);
+    });
+    
+    it("Must become empty if has less than 2 neighbours", () => {
+      let environment: Array<Array<Cell>> = [
+            [new Cell(false), new Cell(false), new Cell(false)],
+            [new Cell(false), new Cell(true), new Cell(true)],
+            [new Cell(false), new Cell(false), new Cell(false)],
+          ],
+          expected: boolean = false,
+          result: boolean = GameOfLife.evaluate(environment, [1, 1]);
+          
+      expect(result).toBe(expected);
+    });
+  });
+  
+  describe("The environment after a day in the game of life", () => {
+    it("should have been updated according to the rules of the game", () => {
+      let toAlive = new Cell(false),
+          stayOverpopulated = new Cell(false),
+          stayEmpty = new Cell(false),
+          becomeOverpopulated = new Cell(true),
+          becomeEmpty = new Cell(true),
+          survive = new Cell(true),
+          
+          start: Array<Array<Cell>> = [
+            [toAlive,           new Cell(true),      new Cell(false), becomeEmpty],
+            [new Cell(true),    becomeOverpopulated, new Cell(true),  new Cell(false)],
+            [stayOverpopulated, new Cell(true),      new Cell(false), new Cell(false)],
+            [new Cell(true),    new Cell(true),      stayEmpty,       new Cell(false)],
+            [survive,           new Cell(false),      new Cell(false), new Cell(false)],
+          ],
+          expected = [
+            [new Cell(true),  new Cell(true),  new Cell(false), new Cell(false)],
+            [new Cell(true),  new Cell(false), new Cell(false), new Cell(false)],
+            [new Cell(false), new Cell(false), new Cell(false), new Cell(false)],
+            [new Cell(true),  new Cell(true),  new Cell(false), new Cell(false)],
+            [new Cell(true),  new Cell(true), new Cell(false), new Cell(false)],
+          ],
+          
+          game = new GameOfLife(start);
+          
+          expect(game.next()).toEqual(expected);
+    })
+  })
     
   describe("Some functions", () => {
     describe("Count neighbours in row function", () => {
@@ -45,5 +119,12 @@ describe("Game of Life kata", () => {
         expect(result).toBe(expected);  
       });
     });
-  });  
+  });
+  
+  describe("Cell class", () => {
+    it("Has an alive public field that can be setted through the constructor", () => {
+      expect(typeof new Cell(false).alive).not.toBe("undefined");
+      expect(new Cell(false).alive).toBe(false);
+    })
+  })
 });
